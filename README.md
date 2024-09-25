7596b39c61864e7ca81b81f92550079fd760fd3f
NDrko <checho.nb@gmail.com>
2024-09-25 15:19:49 -0300

    Modificacion en spotifyControllers y en Readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Repositorio del Grupo 18 de UTN-Laboratorio en Computacion IV
 RECORDATORIO: usar "npm install" para las dependencias y luego correr aplicacion con "node app.js"
 Si tira error husky, poner "npx standard --fix" para arreglar la mayoria de los errores
+
+ Aportes de Alumno Batista:  auth.js - Carpeta Routes/apiRoutes.js - Carpeta controllers/spotifyControllers.js - modif en el .env
+ Aportes de Alumno Cappa: server.js - artistas.js(en carpeta Routes) - artistas.js(en carpeta controllers) - modif en app.js
--- controllers/spotifyControllers.js
+++ controllers/spotifyControllers.js
@@ -30,15 +30,27 @@ const getAlbumById = async (req, res) => {
 }
 
 const searchAlbums = async (req, res) => {
-  const { artist } = req.query
+  const { artist } = req.query // Obtener el nombre del artista desde los query params
+  if (!artist) {
+    return res.status(400).json({ status: 'error', msg: 'Debes proporcionar un artista' })
+  }
   try {
-    const token = await getAccessToken()
-    const response = await axios.get(`https://api.spotify.com/v1/search?q=${artist}&type=album`, {
+    const token = await getAccessToken() // Obtener el token de acceso
+    const response = await axios.get('https://api.spotify.com/v1/search', {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}` // Usar el token para autenticar la solicitud
+      },
+      params: {
+        q: artist, // Pasar el nombre del artista como parte de la búsqueda
+        type: 'album', // Buscar solo álbumes
+        limit: 50 // Limitar los resultados a 50 para cumplir con los requisitos del PDF
       }
     })
-    res.status(200).json({ status: 'ok', data: response.data.albums.items })
+    if (response.data.albums.items.length > 0) {
+      res.json({ status: 'ok', data: response.data.albums.items })
+    } else {
+      res.status(404).json({ status: 'error', msg: 'No se encontraron álbumes para este artista' })
+    }
   } catch (error) {
     res.status(500).json({ status: 'error', msg: 'Error inesperado al obtener la información' })
   }
